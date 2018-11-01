45332b6bf576db37f8b07c76c8b6f8cdff83829b
fjw <1281043316@qq.com>
2018-11-01 10:35:09 +0800

    新增手机号校验

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -12,8 +12,8 @@
         <form>
           <div :class="{on:loginWay}">
             <section class="login_message">
-              <input type="tel" maxlength="11" placeholder="手机号">
-              <button disabled="disabled" class="get_verification">获取验证码</button>
+              <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+              <button :disabled="!rightPhone" class="get_verification" :class="{right_phone: rightPhone}">获取验证码</button>
             </section>
             <section class="login_verification">
               <input type="tel" maxlength="8" placeholder="验证码">
@@ -56,7 +56,13 @@
 export default {
   data () {
     return {
-      loginWay: false // true代表短信登陆, false代表密码
+      loginWay: false, // true代表短信登陆, false代表密码
+      phone: ''// 手机号
+    }
+  },
+  computed: {
+    rightPhone () {
+      return /^1\d{10}$/.test(this.phone)
     }
   }
 }
